069501b0c982a0a3f6ec02afc88af420d9b677b8
aalexsoloveiko <aalexsoloveiko@gmail.com>
2019-03-14 15:21:56 +0300

    Update index.js
    solution

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
-// PLEASE DON'T change function name
+
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    let coinsSet = {};
+  const coinsValue = [50, 25, 10, 5, 1];
+  const coinsName = ['H', 'Q', 'D', 'N', 'P'];
+
+  if ((currency > 0) && (currency <= 10000)) {
+    for (let i = 0; i <= 4; i++) {
+      let total = Math.floor(currency / coinsValue[i]);
+      if (total !== 0) {
+        coinsKit[coinsName[i]] = total;
+        currency %= coinsValue[i];
+      }
+    }
+  }
+
+  if (currency > 10000) {
+    coinsSet['error'] = 'You are rich, my friend! We don\'t have so much coins for exchange';
+  }
+
+  return coinsSet;
 }
